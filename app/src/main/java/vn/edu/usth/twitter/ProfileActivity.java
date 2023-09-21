835fbd6dfe43b8aae7c8c12a3c5b7c9b3f5e9b61
Your Name <you@example.com>
2023-09-21 20:12:19 +0700

    Merge UI and master

--- app/src/main/java/vn/edu/usth/twitter/ProfileActivity.java
+++ app/src/main/java/vn/edu/usth/twitter/ProfileActivity.java
@@ -3,6 +3,9 @@ package vn.edu.usth.twitter;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
 import androidx.appcompat.widget.Toolbar;
 
 public class ProfileActivity extends AppCompatActivity {
@@ -18,4 +21,38 @@ public class ProfileActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
 
+    private boolean liked = false;
+    public void reactPost(View view){
+        ImageButton button = (ImageButton) view;
+        int icon;
+
+        if (liked) {
+            liked = false;
+            icon = R.drawable.heart;
+        }
+        else {
+            liked = true;
+            icon = R.drawable.red_heart;
+        }
+
+        button.setBackgroundResource(icon);
+
+    }
+    private boolean bookmark = false;
+    public void bookmarkPost(View view){
+        ImageButton button = (ImageButton) view;
+        int icon;
+
+        if (bookmark) {
+            bookmark = false;
+            icon = R.drawable.bookmark;
+        }
+        else {
+            bookmark = true;
+            icon = R.drawable.bookmarked;
+        }
+
+        button.setBackgroundResource(icon);
+
+    }
 }
